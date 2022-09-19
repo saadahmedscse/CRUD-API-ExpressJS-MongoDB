7d579453f963591485347784289304a867986ca1
Saad <saadahmedscse@gmail.com>
2022-09-19 14:56:24 +0600

    Added single post api

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+
+const Todo = new mongoose.model("Todo", todoSchema);
 
 //Get all the Todos
 router.get("/get-todos", async (req, res) => {
@@ -13,7 +17,17 @@ router.get("/get-todo/:id", async (req, res) => {
 
 //Post a todo
 router.post("/add", async (req, res) => {
-  //
+  const todo = new Todo(req.body);
+  await todo.save((err, data) => {
+    if (err) {
+      res.status(400).json({ status: "Bad request" });
+    } else {
+      res.json({
+        status: "Todo added successfully",
+        data: data,
+      });
+    }
+  });
 });
 
 //Post multiple todo
