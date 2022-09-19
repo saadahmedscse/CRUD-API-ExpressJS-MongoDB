ef78612ef3b43692d31f8dc3c4c6ac64bf0a37b1
Saad <saadahmedscse@gmail.com>
2022-09-19 14:35:00 +0600

    Imported router for routing

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const todoHandler = require('./routeHandler/todoHandler');
 
-//Start
+//Express app initialization
+const app = express();
+app.use(express.json());
+
+//Database connection with mongoose
+mongoose
+  .connect("mongodb://localhost/todo_app")
+  .then(() => console.log("Database connected successfully"))
+  .catch(() => console.log(err));
+
+//App routes
+app.use('/api', todoHandler);
 
 app.listen(3000, () => {
   console.log(`Listening to port 3000`);
