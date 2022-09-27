20094395769d5508e754e7388c63fc165371879f
Saad <saadahmedscse@gmail.com>
2022-09-27 10:44:21 +0600

    Added socket.io

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const todoHandler = require('./routeHandler/todoHandler');
+const socket = require("socket.io");
+const fs = require("fs");
+const todoHandler = require("./routeHandler/todoHandler");
 
 //Express app initialization
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 //Database connection with mongoose
 mongoose
@@ -13,8 +16,19 @@ mongoose
   .catch(() => console.log(err));
 
 //App routes
-app.use('/api', todoHandler);
+app.use("/api", todoHandler);
 
-app.listen(3000, () => {
-  console.log(`Listening to port 3000`);
+const server = app.listen(3000);
+
+const io = socket(server);
+
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log(`New socket connection: ${socket.id}`);
+
+  socket.on("counter", () => {
+    count++;
+    io.emit("counter", count);
+  });
 });
